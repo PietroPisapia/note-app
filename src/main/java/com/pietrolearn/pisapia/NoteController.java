0d262b9669b33be4bff3a3bc3df1b0b2d418aab1
Pietro Pisapia <pietromariapisapia96@gmail.com>
2023-03-04 16:38:38 +0100

    completa e pronta per il push

--- src/main/java/com/pietrolearn/pisapia/NoteController.java
+++ src/main/java/com/pietrolearn/pisapia/NoteController.java
@@ -2,8 +2,11 @@ package com.pietrolearn.pisapia;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,13 +20,34 @@ public class NoteController {
     }
 
     @GetMapping("/notes")
-    Iterable getNotes(){
+    Iterable<Note> getNotes(){
         return noteRepository.findAll();
     }
+    
+    @GetMapping("/notes/{noteId}")
+    Note getNote(@PathVariable Long noteId){
+        return noteRepository.findById(noteId).orElseThrow();  
+    }
+
 
     @PostMapping("/notes")
     Note createNote(@RequestBody Note newNote){
         return noteRepository.save(newNote);
+    }
+
+    @PutMapping("/notes/{noteId}")
+    Note updateNote(@PathVariable Long noteId, @RequestBody Note notaDto){
+        Note noteUpdate = noteRepository.findById(noteId).orElseThrow();
+        noteUpdate.setTitle(notaDto.getTitle());
+        noteUpdate.setContent(notaDto.getContent());
+        
+        return noteRepository.save(noteUpdate);
+
+    }
+
+    @DeleteMapping("/notes/{noteId}")
+    void deleteNote(@PathVariable Long noteId){
+        noteRepository.delete(noteRepository.findById(noteId).orElseThrow());
     }
 
 }
